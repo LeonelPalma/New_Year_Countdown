75a9361adf797e29404dbfa35caf3f5d659c067c
Leonel Palma <80472480+LeonelPalma@users.noreply.github.com>
2023-03-26 17:05:47 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,52 +1,37 @@
-function timeLeft(endtime){
-var t = Date.parse(endtime) - Date.parse(new Date());
-var seconds = Math.floor( (t/1000) % 60 );
-var minutes = Math.floor( (t/1000/60) % 60 );
-var hours = Math.floor( (t/(1000*60*60)) % 24 );
-var days = Math.floor( t/(1000*60*60*24) );
-return {
-'total': t,
-'days': days,
-'hours': hours,
-'minutes': minutes,
-'seconds': seconds
-};
-};
-$(document).ready(function() {
-var today = new Date();
-var deadline = 'January 1 ' + (today.getFullYear() + 1) + " 00:00:00";
-if (today.getMonth() == 0 && today.getDate() == 1) {
-deadline = 'January 1 ' + (today.getFullYear()) + " 00:00:00";
-};
-$("#header").hover(function() {
-$(this).toggleClass('bluelight');
-});
-$(".clock").hover(function() {
-$(this).toggleClass('bluelight');
-});
-var setClock = function(newyear){
-var timeinterval = setInterval(function(){
-var t = timeLeft(newyear);
-$('#days').text(t.days);
-$('#hours').text(t.hours);
-$('#mins').text(('0' + t.minutes).slice(-2));
-$('#secs').text(('0' + t.seconds).slice(-2));
-if(t.total<=0){
-clearInterval(timeinterval);
-var now = new Date();
-var yearStr = now.getFullYear().toString();
-$('#header').text("Happy New Year!!!");
-$('#days').text(yearStr[0]);
-$('#days-text').text("Happy");
-$('#hours').text(yearStr[1]);
-$('#hours-text').text("New");
-$('#mins').text(yearStr[2]);
-$('#mins-text').text("Year");
-$('#secs').text(yearStr[3]);
-$('#secs-text').text("!!!");
-$('#info').text("Countdown starts again tomorrow!");
+let dayBox = document.getElementById("day-box");
+let hrBox = document.getElementById("hr-box");
+let minBox = document.getElementById("min-box");
+let secBox = document.getElementById("sec-box");
+let endDate = new Date(2024, 0, 1, 00, 00);
+let endTime = endDate.getTime();
+
+function countdown() {
+  let todayDate = new Date();
+  let todayTime = todayDate.getTime();
+  let remainingTime = endTime - todayTime;
+  let oneMin = 60 * 1000;
+  let oneHr = 60 * oneMin;
+  let oneDay = 24 * oneHr;
+
+  let addZeroes = (num) => (num < 10 ? `0${num}` : num);
+
+  if (endTime < todayTime) {
+    clearInterval(i);
+    document.querySelector(
+      ".countdown"
+    ).innerHTML = `<h1>Countdown Has Expired</h1>`;
+  } else {
+    let daysLeft = Math.floor(remainingTime / oneDay);
+    let hrsLeft = Math.floor((remainingTime % oneDay) / oneHr);
+    let minsLeft = Math.floor((remainingTime % oneHr) / oneMin);
+    let secsLeft = Math.floor((remainingTime % oneMin) / 1000);
+
+    dayBox.textContent = addZeroes(daysLeft);
+    hrBox.textContent = addZeroes(hrsLeft);
+    minBox.textContent = addZeroes(minsLeft);
+    secBox.textContent = addZeroes(secsLeft);
+  }
 }
-},1000);
-};
-setClock(deadline);
-});
+
+let i = setInterval(countdown, 1000);
+countdown();
